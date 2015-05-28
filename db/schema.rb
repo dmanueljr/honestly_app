e8ac26b2113d6631ab6885251d721799483d97a8
Daniel Manuel <dmanueljr@gmail.com>
2015-05-28 15:50:41 -0700

    updated users table to reflect new password_digest column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150528222830) do
+ActiveRecord::Schema.define(version: 20150528224301) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "message_id"
@@ -38,8 +38,9 @@ ActiveRecord::Schema.define(version: 20150528222830) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
