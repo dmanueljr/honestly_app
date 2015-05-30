099c12704f4a1fbd505cb76fdf86d19b715987f2
Daniel Manuel <dmanueljr@gmail.com>
2015-05-30 11:05:22 -0700

    temp adds resources for user messages and addresses

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,13 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  get '/new' => 'users#new'
-  get 'users/new' => 'users#new'
+  resources :users do
+    resources :messages
+  end
 
-
+  resources :messages do
+    resources :addresses
+  end
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
