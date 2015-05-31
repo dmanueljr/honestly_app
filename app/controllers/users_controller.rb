4c7852d41e25bf7978f22f59398e19818757870a
Daniel Manuel <dmanueljr@gmail.com>
2015-05-30 22:40:03 -0700

    adds new and create actions in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,26 @@ class UsersController < ApplicationController
   end
 
   def create
+	@user = User.new(user_params)
+	if @user.save
+		redirect_to users_path
+	else
+		render :new
+	end
+  end
+
+  def show
   end
 
   def edit
   end
+
+  def destroy
+  end
+
+private
+	def user_params
+		params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+	end
+
 end
