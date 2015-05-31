eb53edd9ab783a32db2a94e848725b6e65e24fd8
Daniel Manuel <dmanueljr@gmail.com>
2015-05-31 13:58:32 -0700

    adds login routes to sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,10 @@ Rails.application.routes.draw do
   resources :messages do
     resources :addresses
   end
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
