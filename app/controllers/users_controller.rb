bf61d3e4255e86e890c09703f780dbf5ddd365c9
Daniel Manuel <dmanueljr@gmail.com>
2015-06-03 17:57:25 -0700

    adds update and destroy action in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,11 @@ class UsersController < ApplicationController
 
   def create
 	@user = User.new(user_params)
-	if @user.save
-		redirect_to users_path
-	else
-		render :new
-	end
+  	if @user.save
+  		redirect_to users_path
+  	else
+  		render :new
+  	end
   end
 
   def show
@@ -22,9 +22,24 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render "edit"
+    end
   end
 
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to users_path
   end
 
 private
