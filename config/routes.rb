e5fc5ac98b28827de4f66c471b855281dc90e438
Daniel Manuel <dmanueljr@gmail.com>
2015-06-04 10:56:56 -0700

    deletes resources and adds individual routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,45 @@
 Rails.application.routes.draw do
 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'honest#index'
+  root "honest#index"
+
+  #users routes
+  get "/users" => "users#index"
+  post "/users" => "users#create"
+  get "/users/new" => "users#new"
+  get "/users/:id/edit" => "users#edit", as: :edit_user
+  get "/users/:id" => "users#show", as: :user
+  patch "/users/:id" => "users#update"
+  delete "/users/:id" => "users#destroy"
+
+
+  #messages routes
+  get "/messages" => "messages#index"
+  post "/messages" => "messages#create"
+  get "/messages/new" => "messages#new"
+  get "/messages/:id/edit" => "messages#edit", as: :edit_message
+  get "/messages/:id" => "messages#show", as: :message
+  patch "/messages/:id" => "messages#update"
+  delete "/messages/:id" => "messages#destroy"
+
+
+  #addresses routes
+  get "/addresses" => "addresses#index"
+  post "/addresses" => "addresses#create"
+  get "/addresses/new" => "addresses#new"
+  get "/addresses/:id/edit" => "addresses#edit", as: :edit_address
+  get "/addresses/:id" => "addresses#show", as: :address
+  patch "/addresses/:id" => "addresses#update"
+  delete "/addresses/:id" => "addresses#destroy"
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  resources :users do
-    resources :messages
-  end
-
-  resources :messages do
-    resources :addresses
-  end
+  
 
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
@@ -22,9 +47,9 @@ Rails.application.routes.draw do
 
 
   #omniauth
-  get "/auth/:provider/callback" => "sessions#create"
-  get 'auth/logout' => 'sessions#destroy'
-  get 'sessions/show'
+  # get "/auth/:provider/callback" => "sessions#create"
+  # get 'auth/logout' => 'sessions#destroy'
+  # get 'sessions/show'
 
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
