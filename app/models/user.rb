073c0f51b0872db4a9f647e91034ba915840c192
Daniel Manuel <dmanueljr@gmail.com>
2015-06-05 12:40:43 -0700

    adds new user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
+
 	has_many :messages
+	
 	has_secure_password
+
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+  	validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  	validates :password, presence: true, length: { in: 6..20 }
 end
