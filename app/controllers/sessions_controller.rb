55eaa4b394a99eb043cdb8936911e0b04d6e2491
Daniel Manuel <dmanueljr@gmail.com>
2015-06-05 22:08:59 -0700

    changes login redirect to messages path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
 		if user && user.authenticate(params[:login][:password])
 			#log them in (set a cookie)
 			session[:user_id] = user.id.to_s
-			redirect_to user_path(user)
+			redirect_to messages_path
 		else
 			render :new
 		end
